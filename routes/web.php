b2acea7e14bd98f7486ea61722b6a8cb792318e0
Ngon Nguyen <nguyenngon95@gmail.com>
2018-11-04 17:06:26 +0700

    Checkout Order

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['namespace' => 'Watch'], function() {
     Route::resource('product', 'ProductController', ['only' => 'index']);
     Route::resource('cart', 'CartController', ['only' => ['index', 'create', 'update', 'destroy']])->middleware('auth');
+    Route::get('info', 'CartController@inputInfo')->name('cart.info')->middleware('auth');
+    Route::post('confirm', 'CartController@confirm')->name('cart.confirm')->middleware('auth');
+    Route::post('checkout', 'CartController@checkout')->name('cart.checkout')->middleware('auth');
 });
